568957549a17714c56344b840bce878960a6823c
unknown <imtiajahamed6161@gmail.com>
2021-09-03 11:38:09 -0700

    first commit

---
+++ timeoute.js
@@ -0,0 +1,13 @@
+function doSomething(){
+    console.log('i am coding javaScript');
+}
+console.log('first: ami sobar age');
+console.log('second: i am the second person');
+// setTimeout(doSomething, 5000)
+setTimeout (function(){
+    console.log('i am using vs code');
+}, 5000);
+setTimeout(() =>{
+    console.log(' Exploring MDN articles');
+}, 400)
+console.log('third: i am a student');
